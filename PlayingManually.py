fa8d0ee1211129db46b4d6370b629c2a81f049f4
Vincent Palma <vincentpalma2001@gmail.com>
2020-10-29 22:50:56 +0100

    Delete PlayingManually.py

--- PlayingManually.py
+++
@@ -1,5 +0,0 @@
-from PygamePlayCar import CarGame, PolicyFunction, DQN  # noqa: F401
-# need to import PolicyFunction too, else python cannot find it (and also DQN)
-
-g = CarGame()
-g.play_game_manually()
